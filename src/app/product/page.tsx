1653102b0a7589ab2f41aab2c19ab6d007c4cf6e
Ndoll7 <164797060+Ndoll7@users.noreply.github.com>
2025-02-04 05:58:09 -0800

    Update page.tsx

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,36 +1,26 @@
-import Link from 'next/link';
 
-interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
+import Link from 'next/link'
 
-export default async function ProductPage() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    cache: 'no-store', // Fresh data fetch karega
-  });
-
-  const posts: Post[] = await res.json(); // Correct type use karein
-
+export default async function product(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+     const posts= await res.json()
+    
   return (
-    <div className="flex flex-col items-start font-bold space-y-4 text-xl mt-4 pl-7">
-      <h1 className="font-extrabold text-3xl">PRODUCT PAGE</h1>
-      {posts.map((item, i) => (
-        <div key={item.id}>
-          <ul>
-            <li>
-              <Link href={`/product/${item.id}`}>
-                {i + 1}: {item.title}
-              </Link>
-            </li>
-          </ul>
-        </div>
-      ))}
+    <div className='flex flex-col items-start text-bold space-y-4 text-xl mt-4 pl-7'>
+      <h1 className='font-extrabold text-3xl'>PRODUCT PAGE</h1>
+    {
+        posts.map((item:any,i:number)=>{
+          return(
+            <div key={item.id} >
+                <ul>
+                    <li>
+                        <Link href={`/product/${item.id}`}>{i+1}:{item.title}</Link>
+                    </li>
+                </ul>
+            </div>
+          )
+        })
+    }
     </div>
-  );
+  )
 }
-
-
-
